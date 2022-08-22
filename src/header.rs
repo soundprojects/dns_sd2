0d3bd52a50c882ac489d622ef9cf621cdc30d7cc
soundprojects <10196339+soundprojects@users.noreply.github.com>
2022-08-22 04:05:17 +0200

    Add to_bytes() for Header Struct. Needs a fix for multiple bit assignment

--- src/header.rs
+++ src/header.rs
@@ -21,7 +21,7 @@ use bitvec::prelude::*;
 // / +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
 ///
 ///[1035 Section 4.1.1 - Header Format](https://www.rfc-editor.org/rfc/rfc1035#section-4.1.1)
-#[derive(Default, Clone, Debug, Copy)]
+#[derive(Default, Clone, Debug)]
 pub struct Header {
     //ID        A 16 bit identifier assigned by the program that generates any kind of query
     //          This identifier is copied to the corresponding reply and can be used by the requester
@@ -35,7 +35,7 @@ pub struct Header {
     //          1       an inverse query (IQUERY)
     //          2       a server status request (STATUS)
     //          3-15    for future use
-    pub opcode: BitArray,
+    pub opcode: BitVec<usize, Msb0>,
     //AA        Authoritative Answer - this bit is valid in responses,
     //          and specifies that the responding name server is an authority for the domain name in question
     //          Note that the contents of the answer section may have multiple owner names because of aliases
@@ -51,7 +51,7 @@ pub struct Header {
     //          recursive query support is available in the name server}
     pub ra: bool,
     //Z         Reserved for future use. Must be zero in all queries and responses
-    pub z: BitArray,
+    pub z: BitVec<usize, Msb0>,
     //RCODE     Response code - this 4 but field is set as part of responses. The values have the following interpretation:
     //          0   No error condition
     //          1   Format error - The name server was unable to interpret the query.
@@ -63,7 +63,7 @@ pub struct Header {
     //                        For example, a name server mmay not wish to provide the information to the particular requester,
     //                        or a name server may not wish to performm a particular operation (e.g. zone transfer) for particular data
     //          6-15 Reserved for future use
-    pub rcode: BitArray,
+    pub rcode: BitVec<usize, Msb0>,
     //QDCOUNT   an unsigned 16 bit integer specifying the number of entries in the question section
     pub qdcount: u16,
     //ANCOUNT   an unsigned 16 bit integer specifying the nummber of entries in the answer section
@@ -71,4 +71,64 @@ pub struct Header {
     //NSCOUNT   an unsigned 16 bit integer specifying the number of name server resource records in the authority records section
     pub nscount: u16,
     //ARCOUNT   an unsigned 16 bit integer specifying the number of resource records in the additional records section.
+}
+
+impl Header {
+    /// New Header
+    ///
+    /// Returns a `Header` with default values of an empty query
+    pub fn new() -> Self {
+        Header {
+            id: 0,
+            qr: false,
+            opcode: bits![usize, Msb0; 0; 4].to_owned(),
+            aa: false,
+            tc: false,
+            rd: false,
+            ra: false,
+            z: bits![usize, Msb0; 0; 3].to_owned(),
+            rcode: bits![usize, Msb0; 0; 4].to_owned(),
+            qdcount: 0,
+            ancount: 0,
+            nscount: 0,
+        }
+    }
+
+    pub fn parse(data: BitVec<u16, Msb0>) -> Result<Self, String> {
+        todo!();
+    }
+
+    pub fn to_bytes(self) -> BitVec<u16, Msb0> {
+        let mut bytes = bitvec![u16, Msb0; 0; 12];
+
+        //Store ID (Bit 0 .. 15)
+        bytes[0..15].store(self.id);
+
+        // Store QR (Bit 17)
+        bytes.set(16, self.qr);
+
+        //Store OPCODE (Bits 17 .. 20)
+        //TODO FIX
+        // bytes[17..20].store(self.opcode);
+
+        //Store AA (Bit 21)
+        bytes.set(21, self.aa);
+
+        //Store TC (Bit 22)
+        bytes.set(22, self.tc);
+
+        //Store RD (Bit 23)
+        bytes.set(23, self.rd);
+
+        //Store RA (Bit 24)
+        bytes.set(24, self.ra);
+
+        //Store Z (Bits 25 .. 27)
+        //bytes[25..27].store(self.z);
+
+        //Store RCODE (Bits 28 .. 31)
+        //bytes[28..31].store(self.rcode);
+
+        bytes
+    }
 }
